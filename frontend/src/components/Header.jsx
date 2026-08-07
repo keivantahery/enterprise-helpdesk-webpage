bf0dc93f1b7c5749ea83419308d4217f7e69078a
keivantahery <keivan1.tahery@gmail.com>
2026-08-07 16:25:51 +1000

    removing App.css and some other files

--- frontend/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-      <h1>Enterprise Helpdesk</h1>
-    </header>
-  );
-}
-
-export default Header;
--- frontend/src/pages/Tickets.jsx
+++
@@ -1,5 +0,0 @@
-function Tickets() {
-    return <h2>Tickets</h2>;
-}
-
-export default Tickets;
